9d4f02c3e46ea9a8d3b02dcc9dca32eb74ed63cd
Mark Steward <marksteward@gmail.com>
2021-12-14 21:40:00 +0000

    Test for recursive interpolation

--- src/main/java/MyExample.java
+++ src/main/java/MyExample.java
@@ -5,6 +5,8 @@ public class MyExample {
     private static final Logger logger = LogManager.getLogger(MyExample.class);
 
     public static void main(String[] args) {
-        logger.error("${env:SECRET_VALUE:-:}");
+        logger.error("${env:S${upper:e}CRET_VALUE:-:}");
+        logger.error("${env:x:-$}{lower:X}");
+        logger.error("hi: ${upper:{}}", "${env:x:-$}{lower:X}");
     }
 }
